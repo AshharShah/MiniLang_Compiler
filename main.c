6f6a41cae158ae007e1a178781b44d79cc995bc3
AshharShah <ashhar.shah786@gmail.com>
2024-05-15 14:17:52 +0500

    Finished Building Scanner

---
+++ main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+#include "lexer_minilang.h"
+
+int main(int argc, char *argv[]){
+  if(argc != 3){
+    printf("Error: correct syntax: %s <filename.unn> <output_filename>\n", argv[0]);
+    exit(1);
+  }
+
+  FILE *file;
+  file = fopen(argv[1], "r");
+
+  if(!file){
+    printf("ERROR: File not found\n");
+    exit(1);
+  }
+  
+  Token *tokens = lexer(file);
+
+  printf("FINISHED\n");
+}
